749f14e36ed0b6154ea1a3e71827f133aa9454a7
Kishin44 <stefan.stoychev44@gmail.com>
2013-08-02 19:33:19 +0300

    Commit.

--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -12,9 +12,48 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
+        Snake snake;
+
         public Form1()
         {
             InitializeComponent();
+            LinkedList<Rectangle> body = new LinkedList<Rectangle>();
+
+            body.AddFirst(new Rectangle(4, 5, 1, 1));
+            body.AddFirst(new Rectangle(5, 5, 1, 1));
+            body.AddFirst(new Rectangle(6, 5, 1, 1));
+            snake = new Snake(body);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            snake.DrawYourself(e.Graphics);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            snake.Move();
+            Refresh();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case 'a': 
+                    snake.GoLeft();
+                    break;
+                case 'd':
+                    snake.GoRight();
+                    break;
+                case 'w':
+                    snake.GoTop();
+                    break;
+                case 's':
+                    snake.GoBottom();
+                    break;
+            }
         }
     }
 }
---
+++ Snake/Snake.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake : LinkedList<Rectangle>
+    {
+        Point Head = new Point(0, 1);
+        Point Right = new Point(1, 0);
+        Point Left = new Point(-1, 0);
+        Point Top = new Point(0, -1);
+        Point Bottom = new Point(0, 1);
+
+        public Snake(LinkedList<Rectangle> body)
+            :base(body)
+        {
+        }
+        
+        public void DrawYourself(Graphics graphics)
+        {
+            Matrix matrix = new Matrix();
+            matrix.Scale(20.0F, 20.0F);
+            graphics.Transform = matrix;
+
+            graphics.FillRectangles(new SolidBrush(Color.Blue), this.ToArray());
+        }
+
+        public void Move()
+        {
+            Rectangle last = Last.Value;
+            Rectangle first = First.Value;
+
+            last.X = first.X + Head.X;
+            last.Y = first.Y + Head.Y;
+
+            AddFirst(last);
+            RemoveLast();
+        }
+
+        public void GoLeft()
+        {
+            if(!Head.Equals(Right))
+                Head = Left;
+        }
+
+        public void GoRight()
+        {
+            if(!Head.Equals(Left))
+                Head = Right;
+        }
+
+        public void GoTop()
+        {
+            if(!Head.Equals(Bottom))
+                Head = Top;
+        }
+
+        public void GoBottom()
+        {
+            if(!Head.Equals(Top))
+                Head = Bottom;
+        }
+    }
+}
